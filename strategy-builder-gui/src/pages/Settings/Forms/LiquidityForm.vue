0007f20dccb8b1fc30bf2231ea90a297fe7b6e2f
Mike <33065483+Th0rd@users.noreply.github.com>
2022-04-11 11:05:58 +0300

    (feat) Liquidity form logic

--- strategy-builder-gui/src/pages/Settings/Forms/LiquidityForm.vue
+++ strategy-builder-gui/src/pages/Settings/Forms/LiquidityForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="q-gutter-md">
-    <FieldSelect v-bind="exchange" />
-    <FieldSelect v-bind="markets" />
+    <FieldSelect v-bind="exchange" :on-select-update="handleSelectUpdate" />
+    <FieldInputSelect v-bind="markets" :filter="filterMarketsField" />
     <FieldSelect v-bind="token" />
     <FieldInput v-bind="orderAmount" />
     <FieldInput v-bind="spread" />
@@ -18,24 +18,47 @@
   </div>
 </template>
 <script lang="ts">
+import { useExchangesByStrategyName } from 'src/composables/useExchangesByStrategyName';
 import { StrategyName } from 'src/composables/useStrategies';
+import { $exchangeNameMap, ExchangeName } from 'src/stores/exchanges';
 import { defineComponent, ref } from 'vue';
 
 import FieldInput from '../components/FieldInput.vue';
+import FieldInputSelect from '../components/FieldInputSelect.vue';
 import FieldSelect from '../components/FieldSelect.vue';
 import FieldToggle from '../components/FieldToggle.vue';
 import { useForm } from '../composables/useForm';
+import { Select } from '../stores/form.types';
 
 export default defineComponent({
   name: 'LiquidityForm',
-  components: { FieldInput, FieldSelect, FieldToggle },
+  components: { FieldInput, FieldSelect, FieldToggle, FieldInputSelect },
 
   setup() {
     const strategyName = ref(StrategyName.Liquidity);
-    const { fields } = useForm(strategyName);
+    const { fields, updateOptions, updateMarkets, filterMarkets, getMarkets } =
+      useForm(strategyName);
+    const exchanges = useExchangesByStrategyName(strategyName);
+
+    updateOptions('exchange', exchanges.value);
+
+    const handleSelectUpdate = async (val: ExchangeName) => {
+      const markets = await getMarkets($exchangeNameMap[val]);
+      updateMarkets(val, markets);
+    };
+
+    const filterMarketsField = (val: string, update: (callback: () => void) => void) =>
+      filterMarkets(
+        (fields.exchange as Select).value.value as ExchangeName,
+        'markets',
+        val,
+        update,
+      );
 
     return {
       ...fields,
+      handleSelectUpdate,
+      filterMarketsField,
     };
   },
 });
