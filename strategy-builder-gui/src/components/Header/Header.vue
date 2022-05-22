8f88c4e128abfc537c9516619d3af0160967d828
Mike <unclethord@gmail.com>
2022-05-22 05:08:10 +0400

    (refactor) Move logic from useFileHref and Header handleFileUpload to useStrategyFile

--- strategy-builder-gui/src/components/Header/Header.vue
+++ strategy-builder-gui/src/components/Header/Header.vue
@@ -17,107 +17,25 @@
         size="md"
         padding="md lg"
         class="text-uppercase"
-        @click="onClickUpload"
+        @click="file.click()"
       >
         Upload strategy
       </q-btn>
-      <input ref="file" style="display: none" type="file" @change="handleFileUpload" />
+      <input v-show="false" ref="file" type="file" @change="handleFileUpload" />
     </div>
   </q-toolbar>
 </template>
 
 <script lang="ts">
-import { strategyNameFromFileMap } from 'src/pages/Settings/composables/useFileHref';
-import { $fileMap } from 'src/pages/Settings/stores/form';
-import { BtnToggleType } from 'src/pages/Settings/stores/form.types';
-import { defineComponent, getCurrentInstance, Ref, ref } from 'vue';
+import { useStrategyFile } from 'src/composables/useStrategyFile';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   setup() {
-    const imageUrl = ref() as Ref<string>;
     const file = ref<HTMLElement | null>(null);
-    const onClickUpload = () => {
-      file.value?.click();
-    };
+    const { handleFileUpload } = useStrategyFile();
 
-    const handleFileUpload = () => {
-      const fileReader = new FileReader();
-
-      fileReader.addEventListener('load', () => {
-        imageUrl.value = fileReader.result as string;
-
-        const data = Buffer.from(imageUrl.value, 'base64').toString('binary');
-        const repData = data.slice(data.indexOf('template_version:'));
-        const dataArr = repData.split('\n');
-        const obj: { [key: string]: any } = {};
-
-        dataArr.forEach((val) => {
-          const valArr = val.split(':');
-
-          obj[valArr[0].replace(/ /g, '')] = valArr[1].replace(/ /g, '');
-        });
-
-        const stringOrders: string[] = [];
-
-        Object.keys(obj).forEach((key) => {
-          if (key.includes('order_') && obj[key].includes('[')) {
-            stringOrders.push(obj[key]);
-            delete obj[key];
-          }
-        });
-
-        const orders = stringOrders.map((value) => {
-          const orderArr = value.replace(/(\[)|(\])/g, '').split(',');
-          const orderValue = orderArr[0] as BtnToggleType;
-          const orderAmount = Number(orderArr[1]);
-          const orderLevelParam = Number(orderArr[2]);
-
-          const order = {
-            value: orderValue,
-            hint: 'Order hint',
-            orderAmount: {
-              value: orderAmount,
-
-              properties: {
-                placeholder: '0',
-                rightText: '',
-              },
-            },
-            orderLevelParam: {
-              value: orderLevelParam,
-
-              properties: {
-                placeholder: '0',
-                rightText: '',
-              },
-            },
-          };
-          return order;
-        });
-
-        obj.orders = orders;
-
-        const strategyName = strategyNameFromFileMap[obj.strategy];
-        delete obj.strategy;
-        delete obj.template_version;
-        const fileMap = $fileMap[strategyName];
-
-        Object.keys(fileMap).forEach((fileKey) => {
-          Object.keys(obj).forEach((objKey) => {
-            if (fileMap[fileKey] === objKey && objKey !== 'orders') {
-              obj[fileKey] =
-                obj[objKey] === 'true' ? true : obj[objKey] === 'false' ? false : obj[objKey];
-
-              delete obj[objKey];
-            }
-          });
-        });
-
-        localStorage.setItem(strategyName, JSON.stringify(obj));
-      });
-    };
-
-    return { onClickUpload, handleFileUpload, file };
+    return { handleFileUpload, file };
   },
 });
 </script>
---
+++ strategy-builder-gui/src/composables/useStrategyFile.ts
@@ -0,0 +1,158 @@
+import { useForm } from 'src/pages/Settings/composables/useForm';
+import { $fileMap } from 'src/pages/Settings/stores/form';
+import { BtnToggleType, Order } from 'src/pages/Settings/stores/form.types';
+import { StrategyName } from 'src/stores/strategies';
+import { computed, Ref } from 'vue';
+
+const templateVersionMap = {
+  [StrategyName.PureMarketMaking]: 22,
+  [StrategyName.Arbitrage]: 5,
+  [StrategyName.Liquidity]: 3,
+};
+
+export const strategyNameInFileMap = {
+  [StrategyName.PureMarketMaking]: 'pure_market_making',
+  [StrategyName.Liquidity]: 'liquidity_mining',
+  [StrategyName.Arbitrage]: 'arbitrage',
+};
+
+export const strategyNameFromFileMap = {
+  [strategyNameInFileMap[StrategyName.PureMarketMaking]]: StrategyName.PureMarketMaking,
+  [strategyNameInFileMap[StrategyName.Liquidity]]: StrategyName.Liquidity,
+  [strategyNameInFileMap[StrategyName.Arbitrage]]: StrategyName.Arbitrage,
+};
+
+export const useStrategyFile = () => {
+  const getHref = (strategyName: Ref<StrategyName>) => {
+    const { values } = useForm(strategyName);
+    return computed(() => {
+      const valuesObj = Object.keys($fileMap[strategyName.value]).reduce((acc, key) => {
+        const fieldName = $fileMap[strategyName.value][key];
+        let fieldValue: string | number | boolean | string[] =
+          values.value[key] === undefined ? '' : values.value[key];
+
+        if (fieldName === 'orders') {
+          const ordersArr: Order[] = JSON.parse(JSON.stringify(fieldValue));
+          const ordersFormatArr: string[] = [];
+          ordersArr.forEach((val, index) => {
+            ordersFormatArr.push(
+              `order_${index + 1}: [${[
+                String(val.value).toLowerCase(),
+                String(val.orderAmount.value ?? 0),
+                String(val.orderLevelParam.value ?? 0),
+              ]}]`
+                .replace(/,/g, '-')
+                .replace(/(\[)|(\])/g, '='),
+            );
+          });
+          fieldValue = ordersFormatArr;
+        }
+        return {
+          ...acc,
+          [fieldName]: fieldValue,
+        };
+      }, {});
+
+      let iterator = 0;
+      return `data:text/plain,${JSON.stringify(
+        {
+          template_version: templateVersionMap[strategyName.value],
+          strategy: strategyNameInFileMap[strategyName.value],
+          ...valuesObj,
+        },
+        null,
+        1,
+      )
+        .replace(/[{},"]/g, '')
+        .replace(/-/g, ', ')
+        .replace(/(\[)|(\])/g, '')
+        .replace(/=/g, () => {
+          iterator += 1;
+          if (iterator % 2 === 0) {
+            return ']';
+          }
+          return '[';
+        })}`;
+    });
+  };
+
+  const handleFileUpload = (event: Event) => {
+    const fileReader = new FileReader();
+    const files = (event.target as HTMLInputElement).files as FileList;
+
+    fileReader.addEventListener('load', () => {
+      const data = Buffer.from(fileReader.result as string, 'base64').toString('binary');
+      const repData = data.slice(data.indexOf('template_version:'));
+      const dataArr = repData.split('\n');
+      const obj: { [key: string]: string | boolean | object } = {};
+
+      dataArr.forEach((val) => {
+        if (val) {
+          const valArr = val.split(':');
+          obj[valArr[0].replace(/ /g, '')] = valArr[1].replace(/ /g, '');
+        }
+      });
+
+      const stringOrders: string[] = [];
+
+      Object.keys(obj).forEach((key) => {
+        if (key.includes('order_') && (obj[key] as string).includes('[')) {
+          stringOrders.push(obj[key] as string);
+          delete obj[key];
+        }
+      });
+
+      const orders = stringOrders.map((value) => {
+        const orderArr = value.replace(/(\[)|(\])/g, '').split(',');
+        const orderValue = orderArr[0] as BtnToggleType;
+        const orderAmount = Number(orderArr[1]);
+        const orderLevelParam = Number(orderArr[2]);
+
+        const order = {
+          value: orderValue,
+          hint: 'Order hint',
+          orderAmount: {
+            value: orderAmount,
+
+            properties: {
+              placeholder: '0',
+              rightText: '',
+            },
+          },
+          orderLevelParam: {
+            value: orderLevelParam,
+
+            properties: {
+              placeholder: '0',
+              rightText: '',
+            },
+          },
+        };
+        return order;
+      });
+
+      obj.orders = orders;
+
+      const strategyName = strategyNameFromFileMap[obj.strategy as string];
+      delete obj.strategy;
+      delete obj.template_version;
+      const fileMap = $fileMap[strategyName];
+
+      Object.keys(fileMap).forEach((fileKey) => {
+        Object.keys(obj).forEach((objKey) => {
+          if (fileMap[fileKey] === objKey && objKey !== 'orders') {
+            obj[fileKey] =
+              obj[objKey] === 'true' ? true : obj[objKey] === 'false' ? false : obj[objKey];
+
+            delete obj[objKey];
+          }
+        });
+      });
+
+      localStorage.setItem(strategyName, JSON.stringify(obj));
+    });
+    fileReader.readAsDataURL(files[0]);
+  };
+
+  return { getHref, handleFileUpload };
+};
